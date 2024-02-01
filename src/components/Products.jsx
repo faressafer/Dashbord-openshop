784820ccc62687fb0426b722e9598eb4b0e1abc0
FaresOps <faressafer05@gmail.com>
2024-02-01 19:26:25 +0100

    sidebarand header commit

---
+++ src/components/Products.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+
+const Products = () => {
+  return (
+    <div>
+      This is prod
+    </div>
+  )
+}
+
+export default Products
---
+++ src/components/shared/Sidebar.jsx
@@ -0,0 +1,54 @@
+import React from "react";
+import { FcBullish } from "react-icons/fc";
+import { Link, useLocation } from "react-router-dom";
+import { DASHBOARD_SIDEBAR_BOTTOM_LINKS, DASHBOARD_SIDEBAR_LINKS } from "../../lib/const/navigation";
+import classNames from "classnames";
+import { HiOutlineLogout } from "react-icons/hi";
+
+const linkClass =
+  "flex items-center gap-2 font-light px-3 py-2 hover:bg-neutral-700 hover:no-underline active:bg-neutral-600 rounded-sm text-base";
+
+const Sidebar = () => {
+  return (
+    <div className="bg-neutral-800 w-60 p-3 flex flex-col text-white">
+      <div className="flex items-center gap-2 px-1 py-3">
+        <FcBullish fontSize={24} />
+        <span className="text-neutral-100 text-lg">OpenShop</span>
+      </div>
+      <div className="flex-1 py-8 flex flex-col gap-0.5">
+        {DASHBOARD_SIDEBAR_LINKS.map((item) => (
+          <SidebarLink key={item.key} item={item} />
+        ))}
+      </div>
+      <div className="flex flex-col gap-0.5 pt-2 border-t border-neutral-700">
+        {DASHBOARD_SIDEBAR_BOTTOM_LINKS.map((item)=>(
+            <SidebarLink key={item.key} item={item} />
+        ))}
+        <div className={classNames(linkClass, 'cursor-pointer text-red-500')}>
+          <span className="text-xl">
+              <HiOutlineLogout/>
+          </span>
+          Logout
+        </div>
+      </div>
+    </div>
+  );
+};
+
+function SidebarLink({ item }) {
+  const { pathname } = useLocation();
+  return (
+    <Link
+      to={item.path}
+      className={classNames(
+        pathname === item.path ?'text-white bg-neutral-700': 'text-neutral-400',
+        linkClass
+      )}
+    >
+      <span className="text-xl">{item.icon}</span>
+      {item.label}
+    </Link>
+  );
+}
+
+export default Sidebar;
